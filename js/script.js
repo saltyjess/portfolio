16a768ecad02fea64dad5e53ce2ca710ef4ad525
saltyjess <jessielarsen@gmail.com>
2024-08-19 13:08:59 -0400

    Update script.js
    fix light/dark mode

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@ let menuTrigger = document.getElementById("menu-trigger")
 menuTrigger.addEventListener("click", openMe)
 
 let closeMenu = document.getElementById("menu-close")
-closeMenu.addEventListener("click", openMe)
+// closeMenu.addEventListener("click", openMe)
 
 function openMe() {
   let openMenu = document.getElementById("side-menu")
