871120c62ff5565bcc0a4cb58d79c31da2275a25
saltyjess <jessielarsen@gmail.com>
2024-09-04 15:21:57 -0400

    updates

--- js/script.js
+++ js/script.js
@@ -1,22 +1,44 @@
-// //navigation menu open/close
+// navigation menu open/close
+//===========================================
 let menuTrigger = document.getElementById("menu-trigger")
 menuTrigger.addEventListener("click", openMe)
 
 let closeMenu = document.getElementById("menu-close")
-// closeMenu.addEventListener("click", openMe)
+closeMenu.addEventListener("click", openMe)
 
 function openMe() {
   let openMenu = document.getElementById("side-menu")
   openMenu.classList.toggle("hidden")
+  // openMenu.classList.toggle("menu-open") trying to do an annimation
+  console.log(openMenu)
+}
+// Sun moon toggle
+//===========================================
+// document.addEventListener('DOMContentLoaded', function() {
+//   alert("Ready!");
+// }, false);
+
+
+const sunEl = document.getElementById("sun")
+const moonEl = document.getElementById("moon")
+
+moonEl.addEventListener("click", moonOn)
+sunEl.addEventListener("click", sunOn)
+
+function moonOn(){
+  moonEl.classList.add("hidden")
+  sunEl.classList.remove("hidden")
+  document.querySelector("html").setAttribute("data-theme", "dark");
 }
 
-//dark/light mode
-/**
-* Utility function to calculate the current theme setting.
-* Look for a local storage value.
-* Fall back to system setting.
-* Fall back to light mode.
-*/
+function sunOn() {
+  sunEl.classList.add("hidden")
+  moonEl.classList.remove("hidden")
+  document.querySelector("html").setAttribute("data-theme", "light");
+}
+//dark/light mode - keep the theme on all pages and based on your settings
+//===========================================
+
 function calculateSettingAsThemeString({ localStorageTheme, systemSettingDark }) {
   if (localStorageTheme !== null) {
     return localStorageTheme;
@@ -26,32 +48,32 @@ function calculateSettingAsThemeString({ localStorageTheme, systemSettingDark })
   }
   return "light";
 }
+console.log(calculateSettingAsThemeString)
 
-//Utility function to update the button text and aria-label.
-function updateButton({ buttonEl, isDark }) {
-  const newCta = isDark ? "light mode" : "dark mode";
-  // use an aria-label if you are omitting text on the button
-  // and using a sun/moon icon, for example
-  buttonEl.setAttribute("aria-label", newCta);
-  buttonEl.innerText = newCta;
-}
+let button = document.querySelector("[data-theme-toggle]");
 
 //Utility function to update the theme setting on the html tag
 function updateThemeOnHtmlEl({ theme }) {
-  document.querySelector("html").setAttribute("data-theme", theme);
+  if (theme == "dark") {
+    //show the sun & hide the moon
+    moonEl.classList.add("hidden")
+    document.querySelector("html").setAttribute("data-theme", "dark");
+
+  } else{
+    //show the moon & hide the sun
+    sunEl.classList.add("hidden")
+    document.querySelector("html").setAttribute("data-theme", "light");
+}
 }
 
-//On page load:
 //1. Grab what we need from the DOM and system settings on page load
-const button = document.querySelector("[data-theme-toggle]");
-const localStorageTheme = localStorage.getItem("theme");
-const systemSettingDark = window.matchMedia("(prefers-color-scheme: dark)");
+const localStorageTheme = localStorage.getItem("theme");//grabbing the vaiable theme from local storage
+const systemSettingDark = window.matchMedia("(prefers-color-scheme: dark)"); //sees if you prefer dark mode
 
 //2. Work out the current site settings
-let currentThemeSetting = calculateSettingAsThemeString({ localStorageTheme, systemSettingDark });
+let currentThemeSetting = localStorageTheme;
 
 //3. Update the theme setting and button text accoridng to current settings
-updateButton({ buttonEl: button, isDark: currentThemeSetting === "dark" });
 updateThemeOnHtmlEl({ theme: currentThemeSetting });
 
 //4. Add an event listener to toggle the theme
@@ -59,11 +81,10 @@ button.addEventListener("click", (event) => {
   const newTheme = currentThemeSetting === "dark" ? "light" : "dark";
 
   localStorage.setItem("theme", newTheme);
-  updateButton({ buttonEl: button, isDark: newTheme === "dark" });
+  // updateButton({ buttonEl: button, isDark: newTheme === "dark" });
   updateThemeOnHtmlEl({ theme: newTheme });
 
   currentThemeSetting = newTheme;
-  console.log(currentThemeSetting)
 }); 
 
 
